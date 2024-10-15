1189edc6855e8218150747ff824b75339d3525db
Tom Harris Pazhayattil <tomharrisp@gmail.com>
2024-10-15 21:06:32 +0530

    add side guards to Destroy wandering farm animals

--- Assets/Scripts/Volume Scripts/KillVoume.cs
+++ Assets/Scripts/Volume Scripts/KillVoume.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KillVoume : MonoBehaviour {
 
-    [SerializeField] bool isBarn = false;
+    [SerializeField] bool isBarn = false; // AKA shouldKillFarmAnimal
 
     // Start is called before the first frame update
     void Start() {
